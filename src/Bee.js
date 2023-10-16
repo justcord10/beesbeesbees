aa93b8c1ebdccc26718b7e1e29fe255ac2919afb
Justin Cordova <justcord10@gmail.com>
2023-10-16 11:18:58 -0700

    implemented the bee subclass in Bee.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+
+  Grub.call(this); //not needed since a grub is not created with an input argument
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
+
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
 
